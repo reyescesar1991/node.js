6e079aa279ffa430e09a45fa5f21be56a0fb0430
reyescesar0711@gmail.com <reyescesar0711@gmail.com>
2024-10-30 10:41:19 -0400

    practica de comandos para node js

---
+++ input.js
@@ -0,0 +1,11 @@
+const readline = require("readline")
+const prompt = require("prompt-sync")()
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// })
+
+const name = prompt("What is your name ?");
+console.log(name);
+
--- output.js
+++ output.js
@@ -8,10 +8,44 @@ console.log(x,y);
 //%i
 //%o
 
-console.log("I am %s and my age is %d" , "Cesar" , 25);
-console.clear();
-console.count("I am cesar");
-console.count("I am cesar");
-console.count("I am Cesar Augusto");
-console.countReset("I am cesar");
-console.count("I am cesar");
+// console.log("I am %s and my age is %d" , "Cesar" , 25);
+// console.clear();
+// console.count("I am cesar");
+// console.count("I am cesar");
+// console.count("I am Cesar Augusto");
+// console.countReset("I am cesar");
+// console.count("I am cesar");
+
+
+// const sum = () => console.log(`The sum of 2 and 3 is: ${2+3}`);
+// const multiply = () => console.log(`The multiplication of 2 and 3 is: ${2*3}`);
+
+
+// const measureTime = () => {
+
+//     console.time("sum()");
+//     sum();
+//     console.timeEnd("sum()");
+//     console.time("multiply()");
+//     multiply();
+//     console.timeEnd("multiply()");
+// }
+
+// measureTime();
+
+const ProgressBar = require("progress");
+const bar = new ProgressBar("downloading [:bar] :rate/bps :percent :etas", {
+    total: 20,
+});
+
+const timer = setInterval(() => {
+    bar.tick();
+    if(bar.complete){
+
+        clearInterval(timer);
+    }
+}, 100);
+
+const chalk = require("chalk");
+console.log(chalk.green("This is nodejs tutorial"));
+
