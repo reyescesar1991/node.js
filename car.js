389f9de8d761cafbd9f8c0d5700d2ab3bf1dc376
reyescesar0711@gmail.com <reyescesar0711@gmail.com>
2024-10-30 11:23:51 -0400

    uso de los exports en node

---
+++ car.js
@@ -0,0 +1,13 @@
+exports.ford = {
+
+    branch: "Ford",
+    model: "Fiesta"
+}
+
+exports.tesla = {
+
+    branch: "Tesla",
+    model: "Model 3"
+}
+
+// exports.data = {ford, tesla};
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-console.log("this is a nodejs tutorial for beginners");
-console.log("we will cover nodejs with command line");
+// console.log("this is a nodejs tutorial for beginners");
+// console.log("we will cover nodejs with command line");
 // process.exit(0);
 
 // process.exitCode = 1;
+
+
+// const _ = require("lodash");
+// const arr = [1, 4, 6, 8];
+// console.log(_.chunk(arr));
+// console.log(_.last(arr));
+
+// const cowsay = require("cowsay");
+// console.log(cowsay.say({
+//     text: "I am learning NPM module",
+//     e: "00",
+//     t: "U"
+// }));
+
+
+const {tesla, ford} = require("./car");
+console.log(JSON.stringify(tesla, null, 2));
+console.log(JSON.stringify(ford, undefined, 2));
+console.log(tesla);
+console.log(ford);
+
